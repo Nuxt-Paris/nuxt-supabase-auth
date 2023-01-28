164b9c49d0d7768ad78dae9d227f23faedc4bbac
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-01-28 09:38:20 +0000

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ["@/assets/scss/main.scss"],
+  modules: ["@nuxtjs/supabase", "@nuxtjs/tailwindcss"],
+  tailwindcss: {},
+});
