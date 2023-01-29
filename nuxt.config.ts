5b32d86a1314555a5f544d31e42bfe4916963bb0
lemur <lemur@pop-os.localdomain>
2023-01-29 17:16:32 +0100

    feat: add account page and rendering modes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,9 @@ export default defineNuxtConfig({
   css: ["@/assets/scss/main.scss"],
   modules: ["@nuxtjs/supabase", "@nuxtjs/tailwindcss"],
   tailwindcss: {},
+  routeRules: {
+    '/signup': { static: true },
+    '/account': { ssr: true },
+    '/**': { swr: true },
+  }
 });
